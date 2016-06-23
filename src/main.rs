a23a9c4360a7a9930fc566e86cccb3ba1d1b70e9
Regis <boudinot.regis@yahoo.com>
2016-06-22 19:10:45 -0600

    simple template fetcher

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+use std::process::Command;
+
+fn main() {
+    make_html_file(split_it_up(make_a_request("PUT_URL_HERE"))).unwrap();
+}
+
+fn split_it_up(names: Vec<u8>) -> Vec<String> {
+    let names_str = String::from_utf8_lossy(&names);
+    let v: Vec<&str> = names_str.split('\n').collect();
+    let mut new_vec = Vec::new();
+    for i in v {
+        new_vec.push(i.to_string());
+    }
+    new_vec
+}
+
+fn make_a_request(website: &str) -> Vec<u8> {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("curl {}", website))
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    output.stdout
+}
+
+fn make_html_file(incoming_vec: Vec<String>) -> io::Result<()> {
+    let all_markdown = Path::new("./websites/webpage.html");
+
+    let mut buffer = try!(File::create(all_markdown));
+    try!(buffer.write_all(incoming_vec.join("\n").as_bytes()));
+
+    Ok(())
+}
